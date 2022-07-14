77556c31654b9e0976ebb2a67ff7c4d80dc4b42f
K Fitzsimmons <kafitz22@gmail.com>
2022-07-13 20:35:58 -0400

    Cleanup dead code

--- tests/test_distribution_to_gauges.py
+++ tests/test_distribution_to_gauges.py
@@ -2,7 +2,6 @@
 from datetime import datetime
 import pytest
 
-from brownie import ZERO_ADDRESS, chain
 import brownie
 from brownie.network.gas.strategies import LinearScalingStrategy
 
@@ -36,33 +35,6 @@ def setup(dfx, gauge_controller, three_liquidity_gauges_v4, distributor, master_
         1.2842402e16, {'from': new_master_account, 'gas_price': gas_strategy})
 
 
-# def _cast_votes(dfx, gauge_controller, voting_escrow, gauges, master_account, user_accounts):
-#     timestamp = chain.time()
-
-#     # Move 50k DFX to each user wallet and approve for spending by Voting Escrow
-#     total_rewards = 15e22
-#     utils.mint_dfx(dfx, total_rewards, master_account)
-#     for acct in user_accounts:
-#         utils.send_dfx(dfx, 5e22, master_account, acct)
-#         dfx.approve(voting_escrow, 5e22,
-#                     {'from': acct, 'gas_price': gas_strategy})
-
-#     # Provide default values when decorator strategy is commented out
-#     st_deposits = [
-#         5e22,
-#         5e22,
-#         5e22,
-#     ]
-#     st_length = [58, 87, 86]
-#     st_votes = [[4, 3], [1, 4], [0, 3]]
-
-#     utils.deposit_to_ve(voting_escrow, user_accounts,
-#                         st_deposits, st_length, timestamp)
-
-#     # Place votes in bps (10000 = 100.00%)
-#     utils.submit_ve_votes(
-#         gauge_controller, gauges, user_accounts, st_votes)
-
 def _distribute_to_all_gauges(distributor, three_liquidity_gauges_v4, account):
     gauge_addresses = [g.address for g in three_liquidity_gauges_v4]
 
@@ -84,11 +56,6 @@ def test_distribute_rewards(dfx, gauge_controller, distributor, three_liquidity_
         three_liquidity_gauges_v4[0], 1 * 1e18, {'from': master_account, 'gas_price': gas_strategy})
     gauge_controller.change_gauge_weight(
         three_liquidity_gauges_v4[1], 1.1 * 1e18, {'from': master_account, 'gas_price': gas_strategy})
-    # gauge_controller.change_gauge_weight(
-    #     three_liquidity_gauges_v4[2], 10000000000 * 1e18, {'from': master_account, 'gas_price': gas_strategy})
-
-    # _cast_votes(dfx, gauge_controller, voting_escrow, three_liquidity_gauges_v4,
-    #             master_account, user_accounts)
 
     for _ in range(10):
         # Advance chain clock
