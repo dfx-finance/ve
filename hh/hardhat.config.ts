53057a4fee736e68060246a36f6fcb6e45925391
K Fitzsimmons <kafitz22@gmail.com>
2022-10-28 11:47:05 -0400

    Final config changes

--- hh/hardhat.config.ts
+++ hh/hardhat.config.ts
@@ -1,8 +1,47 @@
+require("dotenv").config(); // eslint-disable-line
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+const chainId = 1;
+
+const RpcUrls: Record<string, string> = {
+  1: process.env.ETH_RPC_URL as string,
+  1337: process.env.ETH_RPC_URL as string,
+};
+
+// const ethBlock = 14_443_000; // dfxCad deployed
+// const ethBlock = 14_572_523; // curve dfxCad/cadc staking deployed
+// const ethBlock = 14_725_000; // veDFX deployed
+// const ethBlock = 15_051_000; // euroc/usdc pool deployed
+// const ethBlock = 15_504_570; // ve deployed
+// const ethBlock = 15_742_070; // ammv2 deployed
+// const ethBlock = 15_749_200; // ammv2 redeployed
+// const ethBlock = 15_765_345; // ammv2 seeded
+const ethBlock = 15_816_440; // during epoch 1
+
+const BlockNumbers: Record<string, number> = {
+  1: ethBlock,
+  1337: ethBlock,
+};
+
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
+    hardhat: {
+      chainId,
+      forking: {
+        enabled: true,
+        url: RpcUrls[chainId] || "http://127.0.0.1:8545",
+        blockNumber: BlockNumbers[chainId],
+      },
+      blockGasLimit: 24000000,
+      allowUnlimitedContractSize: true,
+      hardfork: "london",
+    },
+  },
 };
 
 export default config;
