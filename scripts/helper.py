4674ff71b3221253e6f85edb570fd7de84e6fbbe
K Fitzsimmons <kafitz22@gmail.com>
2022-12-22 13:13:38 -0500

    Refactor helpers and utils; remove junk files

--- scripts/helper.py
+++ scripts/helper.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
+from datetime import datetime
 import json
 import time
 
-from brownie import Contract, network, accounts, config
+from brownie import accounts, chain, config, network, web3, Contract
 from brownie.network import show_active, gas_price
 from brownie.network.gas.strategies import LinearScalingStrategy
 
 from dotenv import load_dotenv
-from scripts import addresses
+from scripts import addresses, contracts
 
 load_dotenv()
 
-
+SECONDS_PER_YEAR = 365 * 24 * 60 * 60
 NON_FORKED_LOCAL_BLOCKCHAIN_ENVIRONMENTS = [
     "hardhat",
     "development",
@@ -39,6 +40,105 @@ def get_account(number=None):
     return None
 
 
+def network_info():
+    connected_network = show_active()
+    is_local_network = connected_network in ["ganache-cli", "hardhat", "development"]
+    if is_local_network:
+        gas_price(gas_strategy)
+    return connected_network, is_local_network
+
+
+def get_addresses():
+    connected_network, _ = network_info()
+
+    if connected_network in ["hardhat", "development"]:
+        return addresses.Localhost
+    if connected_network == "polygon-main":
+        return addresses.Polygon
+    if connected_network in ["ethereum", "mainnet"]:
+        return addresses.Ethereum
+
+
+def load_dfx_token():
+    addrs = get_addresses()
+    abi = json.load(open("./tests/abis/Dfx.json"))
+    return Contract.from_abi("DFX", addrs.DFX, abi)
+
+
+###
+### Log to file
+###
+def write_json_log(base_fn, output_data):
+    fp = f"./scripts/{base_fn}_{int(time.time())}.json"
+    with open(fp, "w") as output_f:
+        json.dump(output_data, output_f, indent=4)
+    return fp
+
+
+###
+### Log to console
+###
+def log_distributor_info():
+    _addresses = get_addresses()
+    dfx = contracts.erc20(_addresses.DFX)
+    dfx_distributor = contracts.dfx_distributor(_addresses.DFX_DISTRIBUTOR)
+
+    block_num = web3.eth.block_number
+    block_timestamp = chain[block_num]["timestamp"]
+
+    rewards_enabled = "on" if dfx_distributor.distributionsOn() else "off"
+    distributor_dfx_balance = dfx.balanceOf(dfx_distributor.address)
+    rate = dfx_distributor.rate()
+
+    print("--- Distributor Info -------------------------")
+    print(
+        f"Block time (UTC): {datetime.utcfromtimestamp(block_timestamp)} ({block_num})\n"
+        f"Distributions: {rewards_enabled}\n"
+        f"Distributor mining epoch: {dfx_distributor.miningEpoch()}\n"
+        f"Distributor epoch start time: {datetime.fromtimestamp(dfx_distributor.startEpochTime())}\n"
+        f"Distributor balance (DFX): {dfx_distributor.address} ({distributor_dfx_balance / 1e18})\n"
+        f"Distributor rate (DFX per second): {rate} ({rate / 1e18})\n"
+        f"Distributor rate (DFX per year): {rate * SECONDS_PER_YEAR} ({rate * SECONDS_PER_YEAR / 1e18})\n"
+    )
+
+
+def log_gauges_info(gauge_addresses):
+    _addresses = get_addresses()
+    dfx = contracts.erc20(_addresses.DFX)
+    gauge_controller = contracts.gauge_controller(_addresses.GAUGE_CONTROLLER)
+
+    print("--- Gauges Info -------------------------")
+    weights = [gauge_controller.get_gauge_weight(addr) for _, addr in gauge_addresses]
+    total_weight = sum(weights)
+    for i, weight in enumerate(weights):
+        label, addr = gauge_addresses[i]
+        g = contracts.gauge(addr)
+        raw_dfx_rate = g.reward_data(_addresses.DFX)[3]
+        undistributed_dfx_rewards = (raw_dfx_rate * 604800) / 1e18
+        rewards = [f"{undistributed_dfx_rewards} DFX"]
+        dfx_balance = dfx.balanceOf(g) / 1e18
+
+        weight_pct = weight / total_weight * 100 if total_weight else 0
+        print(
+            f"{label} gauge weight: {weight_pct:.2f}% ({(' / ').join(rewards)}) (DFX balance: {dfx_balance})"
+        )
+    print("")
+
+
+def log_gauge_weights(gauge_addresses):
+    _addresses = get_addresses()
+    gauge_controller = contracts.gauge_controller(_addresses.GAUGE_CONTROLLER)
+
+    print("--- Gauges Weights -------------------------")
+    weights = [gauge_controller.get_gauge_weight(addr) for _, addr in gauge_addresses]
+    for i, weight in enumerate(weights):
+        label, _ = gauge_addresses[i]
+        print(f"{label} gauge weight: {weight}")
+
+
+###
+### Proxy upgrades
+###
 def encode_function_data(initializer=None, *args):
     """Encodes the function call so we can work with an initializer.
     Args:
@@ -90,33 +190,3 @@ def upgrade(
         else:
             transaction = proxy.upgradeTo(newimplementation_address, {"from": account})
     return transaction
-
-
-def load_dfx_token():
-    addrs = get_addresses()
-    abi = json.load(open("./tests/abis/Dfx.json"))
-    return Contract.from_abi("DFX", addrs.DFX, abi)
-
-
-def network_info():
-    connected_network = show_active()
-    is_local_network = connected_network in ["ganache-cli", "hardhat", "development"]
-    if is_local_network:
-        gas_price(gas_strategy)
-    return connected_network, is_local_network
-
-
-def get_addresses():
-    connected_network, _ = network_info()
-
-    if connected_network in ["hardhat", "development"]:
-        return addresses.Localhost
-    if connected_network == "polygon-main":
-        return addresses.Polygon
-    if connected_network in ["ethereum", "mainnet"]:
-        return addresses.Ethereum
-
-
-def write_log(base_fn, output_data):
-    with open(f"./scripts/{base_fn}_{int(time.time())}.json", "w") as output_f:
-        json.dump(output_data, output_f, indent=4)
--- utils/chain.py
+++ utils/chain.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
-import brownie
+from datetime import datetime
+
+from brownie import chain
 from brownie.network.gas.strategies import LinearScalingStrategy
 
 from .constants import WEEK
@@ -7,14 +9,25 @@ from .constants import WEEK
 
 # Setting gas price is always necessary for deploy
 # https://stackoverflow.com/questions/71341281/awaiting-transaction-in-the-mempool
-gas_strategy = LinearScalingStrategy('50 gwei', '200 gwei', 1.3)
+gas_strategy = LinearScalingStrategy("50 gwei", "200 gwei", 1.3)
+
 
 # Fast-forwards chain state to delta (seconds) after (or before if negative)
 # the next distribution epoch starts
+def fastforward_chain_weeks(num_weeks, delta):
+    t0 = chain.time()
+    t1_dt = datetime.fromtimestamp((t0 + num_weeks * WEEK) // WEEK * WEEK + delta)
+    fastforward_chain(t1_dt)
 
 
-def fastforward_chain(num_weeks, delta):
-    t0 = brownie.chain.time()
-    t1 = (t0 + num_weeks * WEEK) // WEEK * WEEK + delta
-    brownie.chain.sleep(t1 - t0)
-    brownie.chain.mine()
+def fastforward_chain(until: datetime):
+    chain.sleep(0)
+    t0 = int(chain.time())
+    chain.sleep(int(until.timestamp()) - t0)
+    chain.mine()
+    t1 = int(chain.time())
+
+    orig = datetime.fromtimestamp(t0)
+    new = datetime.fromtimestamp(t1)
+    print("--- Fast-forward Chain -------------------------")
+    print(f"Fastforwarded chain: {orig} --> {new}\n")
