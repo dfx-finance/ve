be70046c377d2368828d823311d892377c0c6d5d
K Fitzsimmons <kafitz22@gmail.com>
2023-02-28 10:02:01 -0500

    Script distributes to all active gauges

--- scripts/run/distribute_gauge_rewards.py
+++ scripts/run/distribute_gauge_rewards.py
@@ -9,6 +9,17 @@ DEPLOY_ACCT = accounts[0]
 
 addresses = get_addresses()
 
+# fetch all gauge addresses which are not flagged as a killedGauge
+def active_gauges(gauge_controller, dfx_distributor):
+    num_gauges = gauge_controller.n_gauges()
+    all_gauge_addresses = [gauge_controller.gauges(i) for i in range(0, num_gauges)]
+    gauge_addresses = []
+    for addr in all_gauge_addresses:
+        print(addr, dfx_distributor.killedGauges(addr))
+        if dfx_distributor.killedGauges(addr) == False:
+            gauge_addresses.append(addr)
+    return gauge_addresses
+
 
 def main():
     print(
@@ -20,14 +31,7 @@ def main():
     )
     gauge_controller = contracts.gauge_controller(addresses.GAUGE_CONTROLLER)
     dfx_distributor = contracts.dfx_distributor(addresses.DFX_DISTRIBUTOR)
-
-    # Fetch enabled gauge addresses
-    num_gauges = gauge_controller.n_gauges()
-    all_gauge_addresses = [gauge_controller.gauges(i) for i in range(0, num_gauges)]
-    gauge_addresses = []
-    for addr in all_gauge_addresses:
-        if dfx_distributor.killedGauges(addr) == False:
-            gauge_addresses.append(addr)
+    gauge_addresses = active_gauges(gauge_controller, dfx_distributor)
 
     # Distribute gauge rewards
     dfx_distributor.distributeRewardToMultipleGauges(
