eb4cacb75d99fe7769ae69d69004ddf43033658c
K Fitzsimmons <kafitz22@gmail.com>
2023-02-28 09:57:47 -0500

    Update to ammv2s most recent test block

--- hh/hardhat.config.ts
+++ hh/hardhat.config.ts
@@ -22,9 +22,7 @@ const RpcUrls: Record<string, string> = {
 // const ethBlock = 15_941_646; // pre-attack
 // const ethBlock = 15_941_971; // post-attack
 // const ethBlock = 15_983_915; // last DfxDistributor update
-// const ethBlock = 16_240_837; // 2022-12-22 14:27:35 UTC
-// const ethBlock = 16_242_140; // 2022-12-22 18:49:23 UTC
-const ethBlock = 16_585_000; // DFX protocol re-deploy
+const ethBlock = 16_685_597; // ammv2.1 seeded, gauges staked
 
 const BlockNumbers: Record<string, number> = {
   1: ethBlock,
