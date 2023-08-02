8681c4c3ceaa79ea004f38fb16e72a2e07ffd50c
kafitz <kafitz22@gmail.com>
2023-08-02 13:35:26 -0400

    Fix redeploy transfer gauges script

--- scripts/redeploy/1_transfer_liquidity_gauges_v4.py
+++ scripts/redeploy/1_transfer_liquidity_gauges_v4.py
@@ -1,19 +1,14 @@
 #!/usr/bin/env python
-from brownie import accounts, interface
+import brownie
 
 from utils import contracts
+from utils.account import DEPLOY_ACCT, DEPLOY_PROXY_ACCT, impersonate
 from utils.gas import gas_strategy
+from utils.helper import fund_multisigs
 from utils.network import get_network_addresses, network_info
 
 _, is_local_network = network_info()
 addresses = get_network_addresses()
-
-DEPLOY_ACCT = accounts[0] if is_local_network else accounts.load("deployve")
-DEPLOY_PROXY_ACCT = (
-    accounts[1] if is_local_network else accounts.load("deployve-proxyadmin")
-)
-DFX_MULTISIG_ACCT = "0x27E843260c71443b4CC8cB6bF226C3f77b9695AF"
-DFX_PROXY_MULTISIG_ACCT = "0x26f539A0fE189A7f228D7982BF10Bc294FA9070c"
 
 GAUGE_ADDRESSES = [
     ("CADC_USDC", addresses.DFX_CADC_USDC_GAUGE),
@@ -27,25 +22,34 @@ GAUGE_ADDRESSES = [
 
 
 def transfer_all_liquidity_gauges(proxied_gauges):
+    admin = impersonate(addresses.DFX_MULTISIG_0) if is_local_network else DEPLOY_ACCT
+
     for gauge in proxied_gauges:
         gauge.commit_transfer_ownership(
-            DFX_MULTISIG_ACCT, {"from": DEPLOY_ACCT, "gas_price": gas_strategy}
+            addresses.DFX_MULTISIG_0, {"from": admin, "gas_price": gas_strategy}
         )
 
 
 def transfer_all_gauge_proxies(proxied_gauges):
+    proxy_admin = (
+        impersonate(addresses.DFX_MULTISIG_1) if is_local_network else DEPLOY_PROXY_ACCT
+    )
+
     for gauge in proxied_gauges:
-        upgradeable_proxy = interface.IDfxUpgradeableProxy(gauge.address)
+        upgradeable_proxy = brownie.interface.IDfxUpgradeableProxy(gauge.address)
         upgradeable_proxy.changeAdmin(
-            DFX_PROXY_MULTISIG_ACCT,
-            {"from": DEPLOY_PROXY_ACCT, "gas_price": gas_strategy},
+            addresses.DFX_MULTISIG_1,
+            {"from": proxy_admin, "gas_price": gas_strategy},
         )
         print(
-            f"{gauge.address} LiquidityGaugeV4 upgradeable proxy transfer success: {upgradeable_proxy.admin({'from': DFX_PROXY_MULTISIG_ACCT}) == DFX_PROXY_MULTISIG_ACCT}"
+            f"{gauge.address} LiquidityGaugeV4 upgradeable proxy transfer success: {upgradeable_proxy.admin({'from': addresses.DFX_MULTISIG_1}) == addresses.DFX_MULTISIG_1}"
         )
 
 
 def main():
+    if is_local_network:
+        fund_multisigs(DEPLOY_ACCT)
+
     new_gauges = [contracts.gauge(addr) for _, addr in GAUGE_ADDRESSES]
 
     # initiate transfer gauges to multisig
