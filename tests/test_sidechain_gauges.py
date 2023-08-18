a4ef295a6a78b268b4330f6557b7f880c778b60f
K Fitzsimmons <kafitz22@gmail.com>
2023-08-18 13:42:25 -0400

    Add sidechain test skeleton

--- tests/test_sidechain_gauges.py
+++ tests/test_sidechain_gauges.py
@@ -57,27 +57,23 @@ def teardown():
 
 
 def test_l2_gauges(
-    ERC20LP,
-    LiquidityGaugeV4,
-    RootGaugeDelegateOft,
+    RootGaugeCctp,
     DfxUpgradeableProxy,
     DFX,
-    veDFX,
     gauge_controller,
     distributor,
-    veboost_proxy,
     three_gauges,
     deploy_account,
     multisig_0,
     multisig_1,
 ):
     ##
-    ## epoch 0
+    ## epoch 0: do nothing
     ##
     print(f"Epoch {distributor.miningEpoch()}: {DFX.balanceOf(distributor) / 1e18} DFX")
 
     ##
-    ## epoch 1
+    ## epoch 1: distributer rewards to gauges
     ##
     fastforward_chain_weeks(num_weeks=0, delta=0, log=True)
     distributor.distributeRewardToMultipleGauges(
@@ -89,47 +85,103 @@ def test_l2_gauges(
         print(f"{gauge.symbol()}: {DFX.balanceOf(gauge) / 1e18}")
 
     ##
-    ## epoch 2
+    ## epoch 2: deploy L2 gauge, add to gauge controller and distribute rewards
     ##
     fastforward_chain_weeks(num_weeks=0, delta=0, log=True)
 
-    # deploy lpt for test gauge
-    # dev: this won't exist for RootGaugeOft
-    name, symbol = ("L2 LP Token", "L2Pair")
-    lpt = ERC20LP.deploy(
-        name, symbol, 18, 1e9, {"from": deploy_account, "gas_price": gas_strategy}
-    )
-    assert lpt.symbol() == "L2Pair"
-
-    # deploy gauge logic
-    gauge = LiquidityGaugeV4.deploy({"from": deploy_account, "gas_price": gas_strategy})
-
-    # deploy l2 gauge behind proxy
-    gauge_initializer_calldata = gauge.initialize.encode_input(
-        lpt,
-        multisig_0,
+    l2_gauge = deploy_l2_gauge(
+        RootGaugeCctp,
+        DfxUpgradeableProxy,
         DFX,
-        veDFX,
-        veboost_proxy,
+        gauge_controller,
         distributor,
+        deploy_account,
+        multisig_0,
+        multisig_1,
     )
-    dfx_upgradeable_proxy = DfxUpgradeableProxy.deploy(
-        gauge.address,
+
+    add_to_gauge_controller(
+        gauge_controller, l2_gauge, multisig_0, add_placeholder=True
+    )
+
+    # set l2 gauge as a delegate for automating distribution
+    distributor.setDelegateGauge(
+        l2_gauge,
+        l2_gauge,
+        True,
+        {"from": multisig_0, "gas_price": gas_strategy},
+    )
+    print(f"Rewards delegate is set: {distributor.isInterfaceKnown(l2_gauge)}")
+
+    distributor.distributeRewardToMultipleGauges(
+        [*three_gauges, l2_gauge],
+        {"from": multisig_0, "gas_price": gas_strategy},
+    )
+    print(f"Epoch {distributor.miningEpoch()}: {DFX.balanceOf(distributor) / 1e18} DFX")
+    for gauge in [*three_gauges, l2_gauge]:
+        print(f"{gauge.symbol()}: {DFX.balanceOf(gauge) / 1e18}")
+
+    ##
+    ## epoch 3: distribute rewards, do nothing
+    ##
+    fastforward_chain_weeks(num_weeks=0, delta=0, log=True)
+    distributor.distributeRewardToMultipleGauges(
+        [*three_gauges, l2_gauge],
+        {"from": multisig_0, "gas_price": gas_strategy},
+    )
+    print(f"Epoch {distributor.miningEpoch()}: {DFX.balanceOf(distributor) / 1e18} DFX")
+    for gauge in [*three_gauges, l2_gauge]:
+        print(f"{gauge.symbol()}: {DFX.balanceOf(gauge) / 1e18}")
+
+
+##
+## Utils
+##
+def deploy_l2_gauge(
+    RootGaugeCctp,
+    DfxUpgradeableProxy,
+    dfx,
+    gauge_controller,
+    distributor,
+    deploy_account,
+    multisig_0,
+    multisig_1,
+):
+    # deploy gauge logic
+    gauge_implementation = RootGaugeCctp.deploy(
+        {"from": deploy_account, "gas_price": gas_strategy},
+    )
+
+    # deploy gauge proxy and initialize
+    gauge_initializer_calldata = gauge_implementation.initialize.encode_input(
+        "l2-gauge",
+        dfx,
+        gauge_controller,
+        distributor,
+        42161,
+        "0x0000000000000000000000000000000000000001",
+        multisig_0,
+    )
+    proxy = DfxUpgradeableProxy.deploy(
+        gauge_implementation.address,
         multisig_1,
         gauge_initializer_calldata,
         {"from": deploy_account, "gas_price": gas_strategy},
     )
 
-    l2_gauge_proxy = Contract.from_abi(
-        "LiquidityGaugeV4", dfx_upgradeable_proxy, LiquidityGaugeV4.abi
-    )
+    # load gauge interface on proxy for non-admin users
+    gauge_proxy = Contract.from_abi("RootGaugeCctp", proxy, RootGaugeCctp.abi)
+    return gauge_proxy
 
-    # add placeholder gauge type `1` to keep consistent with Angle distributor contract
-    gauge_controller.add_type(
-        "DFX Perpetuals",
-        0,
-        {"from": multisig_0, "gas_price": gas_strategy},
-    )
+
+def add_to_gauge_controller(gauge_controller, gauge, multisig_0, add_placeholder=False):
+    if add_placeholder:
+        # add placeholder gauge type `1` to keep consistent with Angle distributor contract
+        gauge_controller.add_type(
+            "DFX Perpetuals",
+            0,
+            {"from": multisig_0, "gas_price": gas_strategy},
+        )
 
     # add new l2 gauge type to gauge controller
     gauge_controller.add_type(
@@ -139,42 +191,8 @@ def test_l2_gauges(
     )
     # add l2 gauge to gauge controller
     gauge_controller.add_gauge(
-        l2_gauge_proxy,
+        gauge,
         2,
         DEFAULT_GAUGE_WEIGHT,
         {"from": multisig_0, "gas_price": gas_strategy},
     )
-
-    # add l2 gauge delegate
-    l2_delegate = RootGaugeDelegateOft.deploy(
-        DFX,
-        gauge_controller,
-        distributor,
-        multisig_0,
-        {"from": deploy_account, "gas_price": gas_strategy},
-    )
-    distributor.setDelegateGauge(
-        ZERO_ADDRESS, l2_delegate, True, {"from": multisig_0, "gas_price": gas_strategy}
-    )
-
-    distributor.distributeRewardToMultipleGauges(
-        [*three_gauges, l2_gauge_proxy],
-        {"from": multisig_0, "gas_price": gas_strategy},
-    )
-    print(f"Epoch {distributor.miningEpoch()}: {DFX.balanceOf(distributor) / 1e18} DFX")
-    for gauge in [*three_gauges, l2_gauge_proxy]:
-        print(f"{gauge.symbol()}: {DFX.balanceOf(gauge) / 1e18}")
-    print(f"L2 Delegate: {DFX.balanceOf(l2_delegate) / 1e18}")
-
-    ##
-    ## epoch 3
-    ##
-    fastforward_chain_weeks(num_weeks=0, delta=0, log=True)
-    distributor.distributeRewardToMultipleGauges(
-        [*three_gauges, l2_gauge_proxy],
-        {"from": multisig_0, "gas_price": gas_strategy},
-    )
-    print(f"Epoch {distributor.miningEpoch()}: {DFX.balanceOf(distributor) / 1e18} DFX")
-    for gauge in [*three_gauges, l2_gauge_proxy]:
-        print(f"{gauge.symbol()}: {DFX.balanceOf(gauge) / 1e18}")
-    print(f"L2 Delegate: {DFX.balanceOf(l2_delegate) / 1e18}")
