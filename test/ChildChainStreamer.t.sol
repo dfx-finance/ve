ac5d488bdacec70ae991fc0a64e639fb7dbcd880
K Fitzsimmons <kafitz22@gmail.com>
2023-10-24 11:47:00 -0400

    Finish ChildChainStreamer test

--- test/ChildChainStreamer.t.sol
+++ test/ChildChainStreamer.t.sol
@@ -20,7 +20,8 @@ contract ChildChainStreamerTest is Test, Constants, Deploy, Setup {
     address multisig0 = makeAddr("MULTISIG_0");
     address multisig1 = makeAddr("MULTISIG_1");
     address router = address(this);
-    address user = makeAddr("USER");
+    address user0 = makeAddr("USER_0");
+    address user1 = makeAddr("USER_1");
 
     function setUp() public {
         DFX = deployDfx(1e30);
@@ -38,7 +39,6 @@ contract ChildChainStreamerTest is Test, Constants, Deploy, Setup {
         streamer.set_reward_distributor(address(this), address(receiver));
 
         sendToken(address(DFX), 1e23, address(this), address(streamer));
-
         streamer.notify_reward_amount(address(DFX));
 
         // check that token distribution/second will equal the total rewards
@@ -55,13 +55,13 @@ contract ChildChainStreamerTest is Test, Constants, Deploy, Setup {
         streamer.notify_reward_amount(address(DFX));
 
         // deposit lpt to l2 gauge
-        lpt.transfer(user, 1e18);
-        vm.startPrank(user);
+        lpt.transfer(user0, 1e18);
+        vm.startPrank(user0);
         lpt.approve(address(gauge), type(uint256).max);
         gauge.deposit(1e18);
         vm.stopPrank();
 
-        assertEq(gauge.balanceOf(address(user)), 1e18);
+        assertEq(gauge.balanceOf(address(user0)), 1e18);
         assertEq(gauge.reward_tokens(0), address(DFX));
 
         for (uint256 i = 0; i < 3; i++) {
@@ -74,21 +74,81 @@ contract ChildChainStreamerTest is Test, Constants, Deploy, Setup {
             assertApproxEqAbs(DFX.balanceOf(address(gauge)), 1e23, 1e8);
 
             // Claim rewards
-            uint256 availableRewards = gauge.claimable_reward_write(user, address(DFX));
-            vm.prank(user);
-            gauge.claim_rewards(user);
-            uint256 claimedRewards = gauge.claimed_reward(user, address(DFX));
+            uint256 availableRewards = gauge.claimable_reward_write(user0, address(DFX));
+            vm.prank(user0);
+            gauge.claim_rewards(user0);
+            uint256 claimedRewards = gauge.claimed_reward(user0, address(DFX));
 
             // User staked LPT
-            assertEq(gauge.balanceOf(user), 1e18);
+            assertEq(gauge.balanceOf(user0), 1e18);
             // User available rewards
             assertApproxEqAbs(availableRewards, 1e23, 1e8);
             // User claimed rewards
             assertApproxEqAbs(claimedRewards, (i + 1) * 1e23, 1e18);
             // User DFX balance
-            assertApproxEqAbs(DFX.balanceOf(user), (i + 1) * 1e23, 1e18);
+            assertApproxEqAbs(DFX.balanceOf(user0), (i + 1) * 1e23, 1e18);
         }
     }
 
-    function test_TwoStakers() public {}
+    function test_TwoStakers() public {
+        setL2GaugeReward(address(gauge), address(streamer), router, address(DFX), multisig0);
+
+        // give distributor rewards
+        sendToken(address(DFX), 1e23, address(this), address(streamer));
+        streamer.notify_reward_amount(address(DFX));
+
+        // deposit lpt to l2 gauge for user 0
+        lpt.transfer(user0, 1e20);
+        vm.prank(user0);
+        lpt.approve(address(gauge), type(uint256).max);
+        vm.prank(user0);
+        gauge.deposit(1e20);
+
+        // deposit lpt to l2 gauge for user 1
+        lpt.transfer(user1, 1e20);
+        vm.prank(user1);
+        lpt.approve(address(gauge), type(uint256).max);
+        vm.prank(user1);
+        gauge.deposit(1e20);
+
+        for (uint256 i = 0; i < 1; i++) {
+            vm.warp(block.timestamp + WEEK / WEEK * WEEK);
+            sendToken(address(DFX), 1e23, address(this), address(streamer));
+            streamer.notify_reward_amount(address(DFX));
+
+            // DFX balances ~= 1e23
+            assertApproxEqAbs(DFX.balanceOf(address(streamer)), 1e23, 1e8);
+            assertApproxEqAbs(DFX.balanceOf(address(gauge)), 1e23, 1e8);
+
+            // Claim rewards -- user 0
+            uint256 availableRewards0 = gauge.claimable_reward_write(user0, address(DFX));
+            vm.prank(user0);
+            gauge.claim_rewards(user0);
+            uint256 claimedRewards0 = gauge.claimed_reward(user0, address(DFX));
+
+            // User staked LPT
+            assertEq(gauge.balanceOf(user0), 1e20);
+            // User available rewards
+            assertApproxEqAbs(availableRewards0, 5e22, 1e8);
+            // User claimed rewards
+            assertApproxEqAbs(claimedRewards0, (i + 1) * 5e22, 1e18);
+            // User DFX balance
+            assertApproxEqAbs(DFX.balanceOf(user0), (i + 1) * 5e22, 1e18);
+
+            // Claim rewards -- user 1
+            uint256 availableRewards1 = gauge.claimable_reward_write(user1, address(DFX));
+            vm.prank(user1);
+            gauge.claim_rewards(user1);
+            uint256 claimedRewards1 = gauge.claimed_reward(user1, address(DFX));
+
+            // User staked LPT
+            assertEq(gauge.balanceOf(user1), 1e20);
+            // User available rewards
+            assertApproxEqAbs(availableRewards1, 5e22, 1e8);
+            // User claimed rewards
+            assertApproxEqAbs(claimedRewards1, (i + 1) * 5e22, 1e18);
+            // User DFX balance
+            assertApproxEqAbs(DFX.balanceOf(user1), (i + 1) * 5e22, 1e18);
+        }
+    }
 }
