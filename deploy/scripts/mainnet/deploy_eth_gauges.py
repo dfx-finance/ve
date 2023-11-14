f0d1399036c0919403ae6cb1d1729c4efa0cfcb1
K Fitzsimmons <kafitz22@gmail.com>
2023-11-14 14:14:41 -0500

    Updates for deploying LiquidityGaugesV4

--- deploy/scripts/mainnet/deploy_eth_gauges.py
+++ deploy/scripts/mainnet/deploy_eth_gauges.py
@@ -2,10 +2,7 @@
 from brownie import Contract, DfxUpgradeableProxy, LiquidityGaugeV4
 import time
 
-from utils.contracts import (
-    dfx_distributor as _dfx_distributor,
-    veboost_proxy as _veboost_proxy,
-)
+
 from utils.config import (
     DEPLOY_ACCT,
     DEPLOY_PROXY_ACCT,
@@ -22,38 +19,43 @@ deployed = load_outputs(INSTANCE_ID)
 
 
 # deploy gauge logic
-def deploy_implementation(verify_contracts=False):
-    print(f"--- Deploying Liquidity Gauges (v4) contract to {connected_network} ---")
-    gauge_logic = LiquidityGaugeV4.deploy(
-        {"from": DEPLOY_ACCT}, publish_source=verify_contracts
+def deploy_implementation():
+    print(
+        f"--- Deploying LiquidityGaugeV4 implementation contract to {connected_network} ---"
     )
+    gauge_logic = LiquidityGaugeV4.deploy({"from": DEPLOY_ACCT})
+    write_contract(INSTANCE_ID, "liquidityGaugeV4Logic", gauge_logic.address)
+    return gauge_logic
+
+
+def load_implementation():
+    print(
+        f"--- Loading LiquidityGaugeV4 implementation contract on {connected_network} ---"
+    )
+    gauge_logic = LiquidityGaugeV4.at(deployed.read_addr("liquidityGaugeV4Logic"))
     return gauge_logic
 
 
 # deploy gauge as proxy
 def deploy_gauge(gauge_logic: LiquidityGaugeV4, label: str) -> LiquidityGaugeV4:
-    veboost_proxy = _veboost_proxy(deployed.read_addr("veBoostProxy"))
-    dfx_distributor = _dfx_distributor(deployed.read_addr("dfxDistributor"))
-
     # deploy gauge behind proxy
     print(f"--- Deploying LiquidityGaugeV4 proxy contract to {connected_network} ---")
     gauge_initializer_calldata = gauge_logic.initialize.encode_input(
-        existing.read_addr(f"{label}Lp"),
-        DEPLOY_ACCT,
+        existing.read_addr(f"{label}Lpt"),
+        existing.read_addr("multisig0"),
         existing.read_addr("DFX"),
         existing.read_addr("veDFX"),
-        veboost_proxy.address,
-        dfx_distributor.address,
+        deployed.read_addr("veBoostProxy"),
+        deployed.read_addr("dfxDistributor"),
     )
     proxy = DfxUpgradeableProxy.deploy(
         gauge_logic.address,
         DEPLOY_PROXY_ACCT,
         gauge_initializer_calldata,
         {"from": DEPLOY_ACCT},
-        publish_source=VERIFY_CONTRACTS,
+        # publish_source=VERIFY_CONTRACTS,
     )
     write_contract(INSTANCE_ID, f"{label}Gauge", proxy.address)
-    return Contract.from_abi("LiquidityGaugeV4", proxy.address, LiquidityGaugeV4.abi)
 
 
 def main():
@@ -69,16 +71,17 @@ def main():
     verify_deploy_network(connected_network)
     verify_deploy_address(DEPLOY_ACCT)
 
-    gauge_logic = deploy_implementation()
-    if not is_localhost:
-        print("Sleeping after deploy....")
-        time.sleep(3)
+    # gauge_logic = deploy_implementation()
+    # if not is_localhost:
+    #     print("Sleeping after deploy....")
+    #     time.sleep(3)
+    gauge_logic = load_implementation()
 
     deploy_gauge(gauge_logic, "cadcUsdc")
     deploy_gauge(gauge_logic, "eurcUsdc")
     deploy_gauge(gauge_logic, "gbptUsdc")
     deploy_gauge(gauge_logic, "gyenUsdc")
-    # deploy_gauge(gauge_logic, "nzdsUsdc")
+    deploy_gauge(gauge_logic, "nzdsUsdc")
     deploy_gauge(gauge_logic, "trybUsdc")
     deploy_gauge(gauge_logic, "xidrUsdc")
     deploy_gauge(gauge_logic, "xsgdUsdc")
