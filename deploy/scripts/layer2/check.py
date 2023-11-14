cb15ffabbb75a3d20210d9b85458cba9c52c6fa8
K Fitzsimmons <kafitz22@gmail.com>
2023-11-14 16:26:12 -0500

    Uncomment audits

--- deploy/scripts/layer2/check.py
+++ deploy/scripts/layer2/check.py
@@ -33,110 +33,110 @@ GAUGES = {
 
 
 def main():
-    # """
-    # CCIP DFX
-    # """
-    # dfx = clDFX.at(existing.read_addr("DFX"))
-    # Checker._value(dfx.name(), "DFX Token (L2)", "DFX token name")
-    # Checker._value(dfx.symbol(), "DFX", "DFX token symbol")
+    """
+    CCIP DFX
+    """
+    dfx = clDFX.at(existing.read_addr("DFX"))
+    Checker._value(dfx.name(), "DFX Token (L2)", "DFX token name")
+    Checker._value(dfx.symbol(), "DFX", "DFX token symbol")
 
-    # # ChildChainReceiver, ChildChainStreamer, RewardsOnlyGauge
-    # for gauge_key_set in GAUGES[chain.id]:
-    #     receiver_key, streamer_key, gauge_key, lpt_key = gauge_key_set
+    # ChildChainReceiver, ChildChainStreamer, RewardsOnlyGauge
+    for gauge_key_set in GAUGES[chain.id]:
+        receiver_key, streamer_key, gauge_key, lpt_key = gauge_key_set
 
-    #     """
-    #     ChildChainReceiver
-    #     """
-    #     print(
-    #         "-- Checking ChildChainReceiver ({key}): {addr}".format(
-    #             key=receiver_key, addr=deployed.read_addr(receiver_key)
-    #         )
-    #     )
+        """
+        ChildChainReceiver
+        """
+        print(
+            "-- Checking ChildChainReceiver ({key}): {addr}".format(
+                key=receiver_key, addr=deployed.read_addr(receiver_key)
+            )
+        )
 
-    #     receiver = ChildChainReceiver.at(deployed.read_addr(receiver_key))
-    #     # owner
-    #     Checker.address(
-    #         receiver.owner(),
-    #         existing.read_addr("multisig0"),
-    #         "ChildChainReceiver admin",
-    #         debug_addr=existing.read_addr("deployer1"),
-    #     )
-    #     Checker._value(
-    #         receiver.whitelistedSourceChains(ETHEREUM_CHAIN_SELECTOR),
-    #         True,
-    #         "ChildChainReceiver whitelisted source chain",
-    #     )
-    #     Checker._value(
-    #         receiver.whitelistedSenders(existing.read_addr("ccipSenderEth")),
-    #         True,
-    #         "ChildChainReceiver whitelisted sender",
-    #     )
-    #     Checker.address(
-    #         receiver.streamer(),
-    #         deployed.read_addr(streamer_key),
-    #         "ChildChainReceiver streamer",
-    #     )
-    #     Checker.number_gt(receiver.balance(), 0, "ChildChainReceiver gas money")
+        receiver = ChildChainReceiver.at(deployed.read_addr(receiver_key))
+        # owner
+        Checker.address(
+            receiver.owner(),
+            existing.read_addr("multisig0"),
+            "ChildChainReceiver admin",
+            debug_addr=existing.read_addr("deployer1"),
+        )
+        Checker._value(
+            receiver.whitelistedSourceChains(ETHEREUM_CHAIN_SELECTOR),
+            True,
+            "ChildChainReceiver whitelisted source chain",
+        )
+        Checker._value(
+            receiver.whitelistedSenders(existing.read_addr("ccipSenderEth")),
+            True,
+            "ChildChainReceiver whitelisted sender",
+        )
+        Checker.address(
+            receiver.streamer(),
+            deployed.read_addr(streamer_key),
+            "ChildChainReceiver streamer",
+        )
+        Checker.number_gt(receiver.balance(), 0, "ChildChainReceiver gas money")
 
-    #     """
-    #     ChildChainStreamer
-    #     """
-    #     print(
-    #         "-- Checking ChildChainStreamer ({key}): {addr}".format(
-    #             key=streamer_key, addr=deployed.read_addr(streamer_key)
-    #         )
-    #     )
+        """
+        ChildChainStreamer
+        """
+        print(
+            "-- Checking ChildChainStreamer ({key}): {addr}".format(
+                key=streamer_key, addr=deployed.read_addr(streamer_key)
+            )
+        )
 
-    #     streamer = ChildChainStreamer.at(deployed.read_addr(streamer_key))
-    #     # owner
-    #     Checker.address(
-    #         streamer.owner(),
-    #         existing.read_addr("multisig0"),
-    #         "ChildChainStreamer admin",
-    #         debug_addr=existing.read_addr("deployer1"),
-    #     )
-    #     Checker.address(
-    #         streamer.reward_receiver(),
-    #         deployed.read_addr(gauge_key),
-    #         "ChildChainStreamer receiver (gauge)",
-    #     )
-    #     Checker.address(
-    #         streamer.reward_tokens(0),
-    #         existing.read_addr("DFX"),
-    #         "ChildChainStreamer 0th reward (DFX)",
-    #     )
-    #     Checker.number(streamer.reward_count(), 1, "ChildChainStreamer num rewards")
-    #     Checker.address(
-    #         streamer.reward_data(existing.read_addr("DFX"))[0],
-    #         deployed.read_addr(receiver_key),
-    #         "ChildChainStreamer DFX receiver",
-    #     )
+        streamer = ChildChainStreamer.at(deployed.read_addr(streamer_key))
+        # owner
+        Checker.address(
+            streamer.owner(),
+            existing.read_addr("multisig0"),
+            "ChildChainStreamer admin",
+            debug_addr=existing.read_addr("deployer1"),
+        )
+        Checker.address(
+            streamer.reward_receiver(),
+            deployed.read_addr(gauge_key),
+            "ChildChainStreamer receiver (gauge)",
+        )
+        Checker.address(
+            streamer.reward_tokens(0),
+            existing.read_addr("DFX"),
+            "ChildChainStreamer 0th reward (DFX)",
+        )
+        Checker.number(streamer.reward_count(), 1, "ChildChainStreamer num rewards")
+        Checker.address(
+            streamer.reward_data(existing.read_addr("DFX"))[0],
+            deployed.read_addr(receiver_key),
+            "ChildChainStreamer DFX receiver",
+        )
 
-    #     """
-    #     RewardsOnlyGauge
-    #     """
-    #     print(
-    #         "-- Checking RewardsOnlyGauge ({key}): {addr}".format(
-    #             key=gauge_key, addr=deployed.read_addr(gauge_key)
-    #         )
-    #     )
+        """
+        RewardsOnlyGauge
+        """
+        print(
+            "-- Checking RewardsOnlyGauge ({key}): {addr}".format(
+                key=gauge_key, addr=deployed.read_addr(gauge_key)
+            )
+        )
 
-    #     gauge = RewardsOnlyGauge.at(deployed.read_addr(gauge_key))
-    #     # owner
-    #     Checker.address(
-    #         gauge.admin(),
-    #         existing.read_addr("multisig0"),
-    #         "RewardOnlyGauge admin",
-    #         debug_addr=existing.read_addr("deployer1"),
-    #     )
-    #     Checker.address(
-    #         gauge.lp_token(), existing.read_addr(lpt_key), "RewardOnlyGauge lpt"
-    #     )
-    #     Checker.address(
-    #         gauge.reward_tokens(0),
-    #         existing.read_addr("DFX"),
-    #         "RewardOnlyGauge DFX reward",
-    #     )
+        gauge = RewardsOnlyGauge.at(deployed.read_addr(gauge_key))
+        # owner
+        Checker.address(
+            gauge.admin(),
+            existing.read_addr("multisig0"),
+            "RewardOnlyGauge admin",
+            debug_addr=existing.read_addr("deployer1"),
+        )
+        Checker.address(
+            gauge.lp_token(), existing.read_addr(lpt_key), "RewardOnlyGauge lpt"
+        )
+        Checker.address(
+            gauge.reward_tokens(0),
+            existing.read_addr("DFX"),
+            "RewardOnlyGauge DFX reward",
+        )
 
     """
     Migrator
