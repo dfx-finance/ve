d9aa17fa657e1f0324a12ebf6650f595a24790eb
K Fitzsimmons <kafitz22@gmail.com>
2024-02-06 12:12:09 -0500

    Rename deployment contracts

---
+++ deploy/scripts/layer2/1_deploy_child_chain_registry.py
@@ -0,0 +1,13 @@
+def deploy_registry():
+    pass
+
+
+def main():
+    print(
+        (
+            "NOTE: This script expects configuration for:\n"
+            "\t1. Gauge contracts owner\n"
+            "\t2. ChildChainStreamer bytecode\n"
+        )
+    )
+    deploy_registry()
